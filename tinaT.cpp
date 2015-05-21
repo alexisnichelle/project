f7515dceb7405a785d71355c6497747bbbae74c2
tinatafoya <ttafoya@sleipnir>
2015-05-21 09:44:19 -0700

    more platforms

--- tinaT.cpp
+++ tinaT.cpp
@@ -15,31 +15,42 @@ extern "C" {
 void buildlevel(Shape *box, int &number)
 {
     //manually assign individual boxes
-    box[0].width = 400;
-    box[0].height = 70;
-    box[0].center.x = 120 + 5*65;
-    box[0].center.y = 70;
+    box[0].width = 100;
+    box[0].height = 10;
+    box[0].center.x = 100 ;
+    box[0].center.y = 10;
+    number++;
+    box[1].width = 100;
+    box[1].height = 10;
+    box[1].center.x = 200 ;
+    box[1].center.y = 10;
     number++;
 
     //first raised platform 
-    box[1].width = 100;
-    box[1].height = 5;
-    box[1].center.x = 120 + 5*65;
-    box[1].center.y = 240;
+    box[2].width = 100;
+    box[2].height = 10;
+    box[2].center.x = 500;// + 5*65;
+    box[2].center.y = 40;
     number++;
 
     //second raised platform
-    box[2].width = 130;
-    box[2].height = 5;
-    box[2].center.x = 300 + 5*65;
-    box[2].center.y = 340;
+    box[3].width = 100;
+    box[3].height = 10;
+    box[3].center.x = 700;// + 5*65;
+    box[3].center.y = 60;
     number++;
     
     //second raised platform
-    box[3].width = 130;
-    box[3].height = 5;
-    box[3].center.x = 900 + 5*65;
-    box[3].center.y = 340;
+    box[4].width = 100;
+    box[4].height = 10;
+    box[4].center.x = 950;// + 5*65;
+    box[4].center.y = 10;
+    number++;
+    
+    box[5].width = 100;
+    box[5].height = 10;
+    box[5].center.x = 550;
+    box[5].center.y = 280;
     number++;
 }
 ///
